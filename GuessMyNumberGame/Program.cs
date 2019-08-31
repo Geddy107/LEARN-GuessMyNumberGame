3b68dac048a3d771b068af795342d23c847f4896
John Geddeis <JohnGeddeis@outlook.com>
2019-08-31 18:30:25 -0400

    switch statement is being added to allow the computer to guess a number and the user to guess a number.

--- GuessMyNumberGame/Program.cs
+++ GuessMyNumberGame/Program.cs
@@ -6,54 +6,79 @@ namespace GuessMyNumberGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your number range.");
+            Console.WriteLine("What game would you like to play?");
             Console.WriteLine("----------------------------------");
+            Console.WriteLine("1. Have the computer try to guess your number.");
+            Console.WriteLine("2. You try to guess the computer's number.");
 
-            Console.WriteLine("Enter minimum value");
-            int min = int.Parse(Console.ReadLine()); //takes user min range
+            int gameSelect = Console.Read();
 
-            Console.WriteLine("Enter maximum value");
-            int max = int.Parse(Console.ReadLine()); //takes users max range
-
-
-            Console.WriteLine($"Please guess a number between {min} and {max}");
-            int usrInp = int.Parse(Console.ReadLine()); // takes user's guessed number
-
-            Console.Clear();
-            int avg = min - 1; //set to avoid errors on users end
-
-            while (avg != usrInp)
+            switch (gameSelect)
             {
-                avg = (max + min) / 2; //halves the users rang and continues to do so until the users number is reached.
+                case 1:
+                    Console.WriteLine("Please enter your number range.");
+                    Console.WriteLine("----------------------------------");
 
-                Console.WriteLine($"Your number is: {usrInp}");
-                Console.WriteLine($"The Computer guesses: {avg}");
-                Console.WriteLine("------------------------------");
+                    Console.WriteLine("Enter minimum value");
+                    int min = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Is your number lower or higher? \n please select: \n 'H' for higher \n 'L' for lower ");
-                if (avg == usrInp)
-                {
+                    Console.WriteLine("Enter maximum value");
+                    int max = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine($"Please guess a number between {min} and {max}");
+                    int usrInp = int.Parse(Console.ReadLine());
+
                     Console.Clear();
-                    Console.WriteLine($"I guessed it your number is {usrInp}");
+                    int avg = min - 1;
+
+                    while (avg != usrInp)
+                    {
+                        avg = (max + min) / 2;
+
+                        Console.WriteLine($"Your number is: {usrInp}");
+                        Console.WriteLine($"The Computer guesses: {avg}");
+                        Console.WriteLine("------------------------------");
+
+                        Console.WriteLine("Is your number lower or higher? \n please select: \n 'H' for higher \n 'L' for lower ");
+                        if (avg == usrInp)
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"I guessed it your number is {usrInp}");
+                            break;
+                        }
+                        ConsoleKey input = Console.ReadKey().Key;
+
+
+                        if (input == ConsoleKey.L)
+                        {
+                            max = avg;
+                        }
+
+                        else if (input == ConsoleKey.H)
+                        {
+                            min = avg;
+                        }
+
+                    }
                     break;
-                }
-                ConsoleKey input = Console.ReadKey().Key;
+
+                case 2:
 
 
-                if (input == ConsoleKey.L)
-                {
-                    max = avg;
-                }
+                    break;
 
-                else if (input == ConsoleKey.H)
-                {
-                    min = avg;
-                }
 
+                default:
+                    break;
             }
 
-        }
 
+
+
+
+
+        }
 
 
 
